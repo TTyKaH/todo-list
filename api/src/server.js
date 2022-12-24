44164a098b9800a6486d864c9d1b634f60f48e1c
vitaly <vitaly.grudachev@servermania.com>
2022-12-24 14:02:47 +0300

    init todo model WIP

---
+++ api/src/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./models");
+
+const app = express();
+
+
+db.sequelize.sync()
+// sync with tables dropping
+// db.sequelize.sync({ force: true })
+//   .then(() => {
+//     console.log("Drop and re-sync db.");
+//   });
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to your api server" });
+});
+
+require("./routes/todo.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`-----# Server is running on port ${PORT} #-----`);
+});
