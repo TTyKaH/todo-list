eb6d89dddbeab04208e466d61d45e8184c8e16f2
vitaly <vitaly.grudachev@servermania.com>
2022-12-26 22:40:08 +0300

    upd comment

--- api/src/server.js
+++ api/src/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// routes
+// simple welcome route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to your api server" });
 });
