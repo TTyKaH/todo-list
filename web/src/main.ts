37bebbfec1cb7140d9f2ccaaa881e1a958b686ec
Vitaly-ntbk <coolhosta01@gmail.com>
2023-01-15 03:35:05 -0800

    install feather-icons

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,11 +4,14 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import VueFeather from "vue-feather";
+
 import "./assets/styles/index.scss";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.component("VueFeather", VueFeather);
 
 app.mount("#app");
