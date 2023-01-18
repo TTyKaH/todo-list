0b1ea0f9d9cd07eec584890ab2ea883e405c8173
vitaly <vitaly.grudachev@servermania.com>
2023-01-18 23:51:33 +0300

    upd list action

--- web/src/components/Interface/ListActions/ListActionsComponent.vue
+++ web/src/components/Interface/ListActions/ListActionsComponent.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="list-actions">List actions</div>
+  <div class="list-actions">
+    <div class="actions">
+      <TooltipWrapper tooltip="add todo">
+        <VueFeather
+          type="plus-square"
+          @click="toggleSideModal('create-todo')"
+        />
+      </TooltipWrapper>
+    </div>
+    <div>Table settings</div>
+    <ModalWindow
+      v-if="activeModalName === 'create-todo'"
+      @close="toggleSideModal"
+    >
+      sssss
+    </ModalWindow>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ModalWindow from "@/components/UI/modals/ModalWindowComponent.vue";
+
+const activeModalName = ref("");
+
+function toggleSideModal(modalName = "") {
+  console.log("toggleSideModal");
+  activeModalName.value = modalName;
+}
+</script>
 
 <style lang="scss" scoped>
 .list-actions {
+  @apply flex justify-between items-center p-3;
   background: var(--bg-draft);
+
+  .actions {
+    @apply flex items-center;
+  }
 }
 </style>
