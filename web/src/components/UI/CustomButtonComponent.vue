d728d8129d31c1b2d94655540726080f3e3dd017
vitaly <vitaly.grudachev@servermania.com>
2023-01-19 03:00:25 +0300

    create CustomButtonComponent

---
+++ web/src/components/UI/CustomButtonComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="custom-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({});
+</script>
+
+<style lang="scss" scoped>
+.custom-button {
+  @apply py-2 px-5 border rounded;
+  background: var(--draft-btn);
+
+  &:hover {
+    background: var(--draft-btn-hover);
+  }
+}
+</style>
